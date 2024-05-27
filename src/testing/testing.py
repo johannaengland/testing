ef1a310640e95b0baafa585b2830063e8dadcee2
Johanna England <johanna.england@sikt.no>
2024-05-27 12:38:57 +0200

    Add calculate quotient function

--- src/testing/testing.py
+++ src/testing/testing.py
@@ -5,5 +5,10 @@ def calculate_sum(a: int, b: int) -> int:
 def calculate_diff(a: int, b: int) -> int:
     return a - b
 
-def calculate_multiple(a: int, b:int ) -> int:
-    return a*b
+
+def calculate_multiple(a: int, b: int) -> int:
+    return a * b
+
+
+def calculate_quotient(a: int, b: int) -> int:
+    return round(a / b)
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,5 +1,9 @@
-from testing.testing import calculate_sum
+from testing.testing import calculate_sum, calculate_quotient
 
 
 def test_simple():
     assert calculate_sum(1, 2) == 3
+
+
+def test_that_will_fail():
+    assert calculate_quotient(6, 3) == 1
