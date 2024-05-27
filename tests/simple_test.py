27dc7e060302506b4f249f44579db3367f649c34
Johanna England <johanna.england@sikt.no>
2024-05-27 12:47:25 +0200

    Add test for calculate diff

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,5 +1,9 @@
-from testing.testing import calculate_sum
+from testing.testing import calculate_diff, calculate_sum
 
 
 def test_simple():
     assert calculate_sum(1, 2) == 3
+
+
+def test_calculate_diff():
+    assert calculate_diff(4, 2) == 2
