fb781318009fc2a60837f185626342a3c0049497
Johanna England <johanna.england@sikt.no>
2024-06-11 11:45:11 +0200

    Add test for calculate multiple

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,4 +1,4 @@
-from testing.testing import calculate_diff, calculate_sum
+from testing.testing import calculate_diff, calculate_multiple, calculate_sum
 
 
 def test_calculate_sum():
@@ -7,3 +7,7 @@ def test_calculate_sum():
 
 def test_calculate_diff():
     assert calculate_diff(4, 2) == 2
+
+
+def test_calculate_multiple():
+    assert calculate_multiple(3, 4) == 12
